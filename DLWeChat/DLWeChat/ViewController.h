a6578618b032393c156d70221fbbab57049bd9f5
Liqiankun <qiankunli@yahoo.com>
2016-04-19 22:22:36 +0800

    Upload DLWeChat

---
+++ DLWeChat/DLWeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DLWeChat
+//
+//  Created by FT_David on 16/4/18.
+//  Copyright © 2016年 FT_David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DLWeChat/DLWeChat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DLWeChat
+//
+//  Created by FT_David on 16/4/18.
+//  Copyright © 2016年 FT_David. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
